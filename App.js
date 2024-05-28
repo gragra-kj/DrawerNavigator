6eae4033ba1c0882a9efa567b0cad1c78977e300
gragra-kj <muthuigracia27@gmail.com>
2024-05-28 12:49:05 +0300

    drawer navigator

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import DashboardScreen from './screens/DashboardScreen';
+import SettingScreen from './screens/SettingsScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Drawer=createDrawerNavigator();
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name='Dashboard' component={DashboardScreen} options={{
+          title:'My Dashboard',
+          drawerLabel:"Dashboard Label",
+          drawerActiveTintColor:"green",
+          drawerActiveBackgroundColor:"lightblue",
+          drawerContentStyle:{
+            backgroundColor:"#c6cbef"
+          }
+        }}/>
+        <Drawer.Screen name='Settings' component={SettingScreen}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
